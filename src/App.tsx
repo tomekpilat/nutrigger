f8b32559a4f67c39b2573dd711b70b54b320070a
Tomasz Pilat <tomasz.pilat@avanade.com>
2020-11-09 11:56:17 -0500

    small change

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button>Add leaf</button>
+        <button>Remove leaf</button>
+        <button>Send a nudge</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;

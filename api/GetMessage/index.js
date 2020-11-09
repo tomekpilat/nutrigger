d2d88f4f7e760a695101ac4be46e6abf015a75ea
Tomasz Pilat <tomasz.pilat@avanade.com>
2020-11-09 15:56:44 -0500

    Commit

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,4 +1,54 @@
+const { Connection, Request } = require("tedious");
+
 module.exports = async function (context, req) {
+      // Create connection to database
+      const config = {
+        authentication: {
+          options: {
+            userName: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD
+          },
+          type: "default"
+        },
+        server: "nupoc.database.windows.net", 
+        options: {
+          database: "nupoc",
+          encrypt: true
+        }
+      };
+
+      const connection = new Connection(config);
+
+      connection.on("connect", err => {
+        if (err) {
+          console.error(err.message);
+        } else {
+          insertDatabase();
+        }
+      });
+
+      function insertDatabase() {
+        // Read all rows from table
+        const request = new Request(
+          `INSERT INTO Test (NuType, Payload) VALUES ('test1', 'test2')`,
+          (err, rowCount) => {
+            if (err) {
+              console.error(err.message);
+            } else {
+              console.log(`${rowCount} row(s) returned`);
+            }
+          }
+        );
+
+        request.on("row", columns => {
+          columns.forEach(column => {
+            console.log("%s\t%s", column.metadata.colName, column.value);
+          });
+        });
+
+        connection.execSql(request);
+      }
+
     context.res = {
         body: {
             text: "Hello from the API"
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 
+const add = async () => {
+  await fetch('/api/message');
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button>Add leaf</button>
+        <button onClick={add}>Add leaf</button>
         <button>Remove leaf</button>
         <button>Send a nudge</button>
       </header>
